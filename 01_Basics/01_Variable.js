b521d7aa05297699ad99593f531e3f217daf4399
Gagan pratap singh <gaganpratapsingh14@gmail.com>
2026-05-23 20:09:24 +0000

    New File

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let email = "gagan@gmail.com"
+var password = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+console.log(accountId)
---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,8 @@
+"Use Strict"
+/// alert(3+4) we are using NOde JS not web browser 
+let name = "Gagan"
+let age = 24
+let address;
+console.log(name);
+console.log(age);
+console.log(address);
