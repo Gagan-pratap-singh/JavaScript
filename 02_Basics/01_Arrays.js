083b3afbc2212109fccb24f4c11c8f8cfa7cfd03
Gagan pratap singh <gaganpratapsingh14@gmail.com>
2026-05-27 21:39:46 +0000

    Studied Arrays In JS

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,11 @@
+//Array
+const MyArr =[0,1,2,3,4,true,"Gagan"]
+MyArr.push(9)
+MyArr.push(10)
+//MyArr.pop("Gagan")-> This will not work
+MyArr.pop()
+//const myAr2 = new array(1,2,3)
+
+
+console.log(MyArr)
+//console.log(myAr2.length())
