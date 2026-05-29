913a2ab6e03ce52449c48c4cac704ba74efbcae9
Gagan pratap singh <gaganpratapsingh14@gmail.com>
2026-05-29 21:22:30 +0000

    Objects_part_1

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,14 @@
+//Singleton 
+//Constructor se singleton banega outherwise nahi 
+
+//Objects literals
+//Object.create this method is singleton,this is using constroctor
+const JsUser ={
+    name : "Gagan ", //We can define keys and values
+    age : 24,
+    email : "hahah@gmail.com",
+    IsLogedIn: false,
+    lastLogIn_Days : ["Monday","Tuesday"]
+} //Objects literals
+console.log(JsUser)
+console.log(JsUser.email)
