2b4ee8f8aab3eff4291c11591d98235098d696e4
Gagan pratap singh <gaganpratapsingh14@gmail.com>
2026-05-31 11:38:35 +0000

    Adding two objects

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -1,4 +1,24 @@
 //const tinderUser = new Object(); //SingleTon Objects
-const tinderUser = {}  //Non-SingleTon object
+// const tinderUser = {}  //Non-SingleTon object
+// tinderUser.id = 123
+// tinderUser.name = "Deepa"
+// tinderUser.isLogged = false
 
-console.log(typeof tinderUser);
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "hahah@gmail.com",
+    fullName : {
+        FirstName : "deep",
+        lastName : "sarma"
+    } 
+}
+//console.log(regularUser.fullName.FirstName)
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"c",4:"d"}
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 ={...obj1,...obj2}
+console.log(obj3)
